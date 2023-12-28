1c46190ca0a8a3003f355e140ec6e5e4412f5626
Xuzker <gvozdikov@sfedu.ru>
2023-12-28 19:16:33 +0300

    add summary

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from yandex_tracker_client import TrackerClient
 client = TrackerClient(token='y0_AgAAAAAZDvXWAAsP2wAAAAD2NHAkV775oI5KSLKqARWteiTqurDWYD8', cloud_org_id='bpfndimlbi46t0cd4jo0')
 client.issues.create(
     queue="TEAMCITYBUILDFA",
-    summary="",
+    summary="new task",
     assignee="vyacheslavgvozdikov",
     description='opisanie'
 )
